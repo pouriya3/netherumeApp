0488b9e10450cc9a04059fc20d9353648212f1ff
pouriya3 <blockchain.po@palgam.com>
2024-09-21 15:14:28 +0300

    bet table data hash and sign completed

--- Program.cs
+++ Program.cs
@@ -12,21 +12,31 @@ using Nethereum.HdWallet;
 
 class Encoder
 {
-    static long timestamp => DateTimeOffset.UtcNow.ToUnixTimeSeconds();
-    public static string EncodeAndHashForBetTables(string address, uint betAmountInTether, bool isTournamnet)
+    static long TimeStamp => DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+    public static string EncodeAndHashForBetTables(string userAddress, string competitor, string tableAddress, uint betAmountInTether, uint nonce)
     {
         var abiEncode = new ABIEncode();
 
-        BigInteger formattedTime = new BigInteger((long)Math.Floor(timestamp / (isTournamnet ? 1000.0 : 20.0)));
+        BigInteger formattedTime = new BigInteger((long)Math.Floor(TimeStamp / 30.0));
 
         BigInteger betAmountWithDecimals = new BigInteger(betAmountInTether) * 1000000;
-        if (address.StartsWith("0x"))
-        {
-            address = address.Substring(2);
-        }
 
-        var encodedValue = abiEncode.GetABIEncoded([new ABIValue("address", address), new ABIValue("uint256", betAmountWithDecimals), new ABIValue("uint256", formattedTime)]).ToHex();
+
+        userAddress = userAddress.Substring(2);
+        competitor = competitor.Substring(2);
+        tableAddress = tableAddress.Substring(2);
+
+        var encodedValue = abiEncode.GetABIEncoded([
+            new ABIValue("address", userAddress),
+            new ABIValue("address", competitor),
+            new ABIValue("address", tableAddress),
+            new ABIValue("uint256", betAmountWithDecimals),
+            new ABIValue("uint64", nonce),
+            new ABIValue("uint256", formattedTime),
+            ]).ToHex();
+
         Console.WriteLine("Encoded Value: " + encodedValue);
+
         // encodedValue = encodedValue.Substring(24);
         var hash = new Sha3Keccack().CalculateHash(encodedValue.HexToByteArray());
 
@@ -57,7 +67,7 @@ class Encoder
     {
         string hash = "0xdbaaa4203ec767d6c9ac1cb78fc705bcd58b5ea0aded824b0cb0000000000000";
 
-        var hexTime = new BigInteger(timestamp);
+        var hexTime = new BigInteger(TimeStamp);
         var timeByteArray = hexTime.ToByteArray();
         var hexTimeBytes = timeByteArray.ToHex();
         hash = hash.Substring(0, hash.Length - hexTimeBytes.Length);
@@ -88,24 +98,29 @@ class Program
     static void Main(string[] args)
     {
 
-        var mnemonic = "flock rude kingdom almost bike attitude buyer initial skull anxiety decade also";
-
+        var mnemonic = "..."; // 12 word mnemonic
         var wallet = new Wallet(mnemonic, null);
 
         var account = wallet.GetAccount(0);
 
-        // var signer = new EthereumMessageSigner();
+        // // var signer = new EthereumMessageSigner();
         Console.WriteLine("Address: " + account.Address);
-        // account.Address should be smart Account address
+        // // account.Address should be smart Account address
 
-        string hash = Encoder.EncodeAndHashForTournament(account.Address,account.Address ,1,1);
+        string hash = Encoder.EncodeAndHashForBetTables(account.Address, "0x8cb7051a62689241366Dc685625036cd7f81cd06",
+         "0x092eb9d061167FC931805a36ec8621E805553A7F", 1, 3);
         Console.WriteLine("hash for betTables: " + hash);
 
-
-        // signature should send for betTable process
         var signature = SignHash.Sign(hash, mnemonic);
 
         Console.WriteLine("Signed Hash for betTables: " + signature);
+
+        // string hash = Encoder.EncodeAndHashForTournament(account.Address, account.Address, 1, 1);
+        // Console.WriteLine("hash for betTables: " + hash);
+
+
+        // // signature should send for betTable process
+        // var signature = SignHash.Sign(hash, mnemonic);
 
         // var jwtHash = Encoder.hashForJwt();
         // Console.WriteLine("JWT hash  :" + jwtHash);
